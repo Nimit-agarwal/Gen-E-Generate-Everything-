28a0b1ca071a66e275193030469fbc60c48c8980
Nimit Agarwal <140658492+Nimit-agarwal@users.noreply.github.com>
2024-04-27 19:51:02 +0530

    Update crisp-chat.tsx

--- components/crisp-chat.tsx
+++ components/crisp-chat.tsx
@@ -5,7 +5,7 @@ import { Crisp } from "crisp-sdk-web";
 
 export const CrispChat = () => {
   useEffect(() => {
-    Crisp.configure("8254f25a-eca0-421e-b4de-2bf655c03887");
+    Crisp.configure("4d817a6a-8044-436a-81f0-049da73849ea");
   }, []);
 
   return null;
